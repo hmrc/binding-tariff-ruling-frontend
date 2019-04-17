315a785b9e72f22d069bb06a79baf5e5e7e4af19
Ed Wilson <4864094+ed0906@users.noreply.github.com>
2019-04-17 13:52:37 +0100

    BAU - Dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,9 +4,9 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-play-25"          % "4.10.0",
+    "uk.gov.hmrc"             %% "bootstrap-play-25"          % "4.11.0",
     "uk.gov.hmrc"             %% "govuk-template"             % "5.31.0-play-25",
-    "uk.gov.hmrc"             %% "play-ui"                    % "7.38.0-play-25",
+    "uk.gov.hmrc"             %% "play-ui"                    % "7.39.0-play-25",
     "uk.gov.hmrc"             %% "play-json-union-formatter"  % "1.5.0",
     "uk.gov.hmrc"             %% "simple-reactivemongo"       % "7.16.0-play-25"
   )
@@ -21,9 +21,9 @@ object AppDependencies {
     "org.pegdown"             %  "pegdown"                  % "1.6.0"          % scope,
     "org.scalatest"           %% "scalatest"                % "3.0.4"          % scope,
     "org.scalatestplus.play"  %% "scalatestplus-play"       % "2.0.1"          % scope,
-    "uk.gov.hmrc"             %% "hmrctest"                 % "3.6.0-play-25"  % scope,
-    "uk.gov.hmrc"             %% "http-verbs-test"          % "1.4.0-play-25"  % scope,
-    "uk.gov.hmrc"             %% "reactivemongo-test"       % "4.10.0-play-25" % scope,
+    "uk.gov.hmrc"             %% "hmrctest"                 % "3.8.0-play-25"  % scope,
+    "uk.gov.hmrc"             %% "http-verbs-test"          % "1.6.0-play-25"  % scope,
+    "uk.gov.hmrc"             %% "reactivemongo-test"       % "4.13.0-play-25" % scope,
     "uk.gov.hmrc"             %% "service-integration-test" % "0.6.0-play-25"  % scope
   )
 
