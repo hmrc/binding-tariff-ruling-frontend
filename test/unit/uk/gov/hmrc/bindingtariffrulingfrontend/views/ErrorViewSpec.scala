989be15a035c57655512fdb3e4d0e0b0a0413067
Gorkem Unsal <gorkem.unsal@accenture.com>
2021-03-24 14:33:48 +0000

    DIT-2796 increase unit test coverage

---
+++ test/unit/uk/gov/hmrc/bindingtariffrulingfrontend/views/ErrorViewSpec.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.bindingtariffrulingfrontend.views
+import uk.gov.hmrc.bindingtariffrulingfrontend.views.html.error
+
+class ErrorViewSpec extends ViewSpec {
+
+  val errorPage: error = app.injector.instanceOf[error]
+
+
+  "Error View" should {
+    "render error page with correct text" in {
+
+     val doc =  view(errorPage("Title", "Heading", "Message"))
+
+      doc.text() should include("Title")
+      doc.text() should include("Heading")
+      doc.text() should include("Message")
+    }
+  }
+}
---
+++ test/unit/uk/gov/hmrc/bindingtariffrulingfrontend/views/SearchResultsViewSpec.scala
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.bindingtariffrulingfrontend.views
+
+import java.time.Instant
+
+import uk.gov.hmrc.bindingtariffrulingfrontend.connector.model.FileMetadata
+import uk.gov.hmrc.bindingtariffrulingfrontend.model.{Paged, Ruling}
+import uk.gov.hmrc.bindingtariffrulingfrontend.utils.Dates
+import uk.gov.hmrc.bindingtariffrulingfrontend.views.ViewMatchers._
+import uk.gov.hmrc.bindingtariffrulingfrontend.views.html.components.search_results
+
+class SearchResultsViewSpec extends ViewSpec {
+
+  override protected val testMessages: Map[String, Map[String, String]] =
+    Map(
+      "default" -> Map(
+        "search.results.ruling.commodityCode.newTab" -> "(opens in new tab)",
+        "search.results.ruling.description"          -> "Description",
+        "search.results.ruling.keywords"             -> "Keywords",
+        "search.results.ruling.expiryDate"           -> "Expiry date",
+        "search.results.ruling.commodityCode"        -> "Commodity code",
+        "search.results.ruling.images"               -> "Images",
+        "search.results.ruling.view"                 -> "View ruling {0}"
+      ))
+
+  val ruling = Ruling(
+    reference            = "reference",
+    bindingCommodityCode = "bindingCommodityCode",
+    effectiveStartDate   = Instant.now,
+    effectiveEndDate     = Instant.now,
+    justification        = "justification",
+    goodsDescription     = "goodsDescription",
+    keywords             = Set("k1", "k2", "k3"),
+    attachments          = Seq("f1", "f2", "f3"),
+    images               = Seq("id1", "id2")
+  )
+
+  val fileMetaData = Map(
+    "id1" -> FileMetadata("id1", Some("image1"), None, Some("url"), true),
+    "id2" -> FileMetadata("id2", Some("image1"), None, Some("url"), true),
+    "f1"  -> FileMetadata("f1", Some("file1"), None, None, false),
+    "f2"  -> FileMetadata("f2", Some("file2"), None, None, false)
+  )
+
+  val pagedRuling = Paged(Seq(ruling))
+
+  val searchResultsView = app.injector.instanceOf[search_results]
+
+  "Search Results View" should {
+
+    "render correct text fields" in {
+
+      val doc = view(searchResultsView(None, pagedRuling, fileMetaData))
+
+      doc.text() should include(s"${ruling.bindingCommodityCode} (opens in new tab)")
+
+      doc.text() should include(s"Expiry date")
+      doc.text() should include(Dates.format(ruling.effectiveEndDate))
+
+      doc.text() should include(s"Commodity code")
+      doc.text() should include(s"${ruling.bindingCommodityCode} (opens in new tab)")
+
+      doc.text() should include(s"Description")
+      doc.text() should include(s"${ruling.goodsDescription}")
+
+      doc.text() should include(s"Keywords")
+      ruling.keywords.map(keyword => doc.text() should include(keyword))
+    }
+
+    "render images with correct href" in {
+
+      val doc = view(searchResultsView(None, pagedRuling, fileMetaData))
+
+      doc.text() should include(s"Images")
+      doc.getElementById("search_results-list-0") should containElementWithAttribute(
+        "href",
+        s"/search-for-advance-tariff-rulings/ruling/${ruling.reference}/image/id1")
+      doc.getElementById("search_results-list-0") should containElementWithAttribute(
+        "href",
+        s"/search-for-advance-tariff-rulings/ruling/${ruling.reference}/image/id2")
+    }
+
+    "render with correct href link" in {
+
+      val doc = view(searchResultsView(None, pagedRuling, fileMetaData))
+
+      doc.text() should include(s"View ruling ${ruling.reference}")
+      doc.getElementById("search_results-list-0") should containElementWithAttribute(
+        "href",
+        s"/search-for-advance-tariff-rulings/ruling/${ruling.reference}")
+    }
+  }
+}
