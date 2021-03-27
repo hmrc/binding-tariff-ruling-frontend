c14324c1fa77319949468b448ec95369e9e3b001
Scala Steward <davidgregory084@gmail.com>
2021-03-27 13:41:06 +0000

    Update simple-reactivemongo to 8.0.0-play-28

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"          %% "bootstrap-frontend-play-27" % "3.2.0",
-    "uk.gov.hmrc"          %% "simple-reactivemongo"       % "7.31.0-play-27",
+    "uk.gov.hmrc"          %% "simple-reactivemongo"       % "8.0.0-play-28",
     "uk.gov.hmrc"          %% "play-json-union-formatter"  % "1.12.0-play-27",
     "uk.gov.hmrc"          %% "play-allowlist-filter"      % "0.2.0-play-27",
     "uk.gov.hmrc"          %% "play-frontend-govuk"        % "0.56.0-play-27",
