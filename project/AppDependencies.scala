35b608e88ad67c172906225992b9c1b7eb7eb634
Scala Steward <davidgregory084@gmail.com>
2021-03-27 13:34:32 +0000

    Update scalatestplus-play to 5.1.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -27,7 +27,7 @@ object AppDependencies {
     "org.jsoup"              % "jsoup"                     % "1.13.1"         % scope,
     "org.pegdown"            % "pegdown"                   % "1.6.0"          % scope,
     "org.scalatest"          %% "scalatest"                % "3.0.9"          % scope,
-    "org.scalatestplus.play" %% "scalatestplus-play"       % "3.1.3"          % scope,
+    "org.scalatestplus.play" %% "scalatestplus-play"       % "5.1.0"          % scope,
     "org.scalacheck"         %% "scalacheck"               % "1.14.3"         % scope,
     "uk.gov.hmrc"            %% "http-verbs-test"          % "1.8.0-play-27"  % scope,
     "uk.gov.hmrc"            %% "service-integration-test" % "0.13.0-play-27" % scope,
