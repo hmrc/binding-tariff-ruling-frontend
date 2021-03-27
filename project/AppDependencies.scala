c97f8c928ab5177b8d3a9aee5c4123a2bab19319
Scala Steward <davidgregory084@gmail.com>
2021-03-27 13:36:57 +0000

    Update play-allowlist-filter to 1.0.0-play-28

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
     "uk.gov.hmrc"          %% "bootstrap-frontend-play-27" % "3.2.0",
     "uk.gov.hmrc"          %% "simple-reactivemongo"       % "7.31.0-play-27",
     "uk.gov.hmrc"          %% "play-json-union-formatter"  % "1.12.0-play-27",
-    "uk.gov.hmrc"          %% "play-allowlist-filter"      % "0.2.0-play-27",
+    "uk.gov.hmrc"          %% "play-allowlist-filter"      % "1.0.0-play-28",
     "uk.gov.hmrc"          %% "play-frontend-govuk"        % "0.56.0-play-27",
     "uk.gov.hmrc"          %% "play-frontend-hmrc"         % "0.34.0-play-27",
     "uk.gov.hmrc"          %% "mongo-lock"                 % "7.0.0-play-27",
