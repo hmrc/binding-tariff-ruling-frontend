6c65f8740a8e014e95f2cdb7bce275b3438543f1
Scala Steward <davidgregory084@gmail.com>
2021-03-31 14:02:39 +0100

    Update play-frontend-hmrc to 0.56.0-play-27

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
     "uk.gov.hmrc"          %% "play-json-union-formatter"  % "1.12.0-play-27",
     "uk.gov.hmrc"          %% "play-allowlist-filter"      % "0.2.0-play-27",
     "uk.gov.hmrc"          %% "play-frontend-govuk"        % "0.56.0-play-27",
-    "uk.gov.hmrc"          %% "play-frontend-hmrc"         % "0.34.0-play-27",
+    "uk.gov.hmrc"          %% "play-frontend-hmrc"         % "0.56.0-play-27",
     "uk.gov.hmrc"          %% "mongo-lock"                 % "7.0.0-play-27",
     "org.typelevel"        %% "cats-core"                  % "2.2.0",
     "com.digitaltangible"  %% "play-guard"                 % "2.4.0",
