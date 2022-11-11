5edc940eb2717e5684766a329f160998e8d89209
Adrian Jones <14924378+Nboaram@users.noreply.github.com>
2022-11-11 15:38:01 +0000

    [DDCE-3516] Removing presigned S3 Urls and dependency upgrades (#94)
    * [DDCE-3516] Removing presigned S3 Urls and dependency upgrades
    
    * [DDCE-3516] Test for missing files

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,18 +6,18 @@ object AppDependencies {
   val scope = "test"
 
   private val hmrcMongoPlayVersion = "0.73.0"
-  private val silencerVersion      = "1.7.10"
+  private val silencerVersion      = "1.7.12"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28" % "7.3.0",
+    "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28" % "7.11.0",
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"         % hmrcMongoPlayVersion,
     "uk.gov.hmrc"                  %% "play-json-union-formatter"  % "1.15.0-play-28",
     "uk.gov.hmrc"                  %% "play-allowlist-filter"      % "1.1.0",
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc"         % "3.27.0-play-28",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc"         % "3.32.0-play-28",
     "org.typelevel"                %% "cats-core"                  % "2.8.0",
     "com.digitaltangible"          %% "play-guard"                 % "2.5.0",
     "org.quartz-scheduler"         % "quartz"                      % "2.3.2",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.13.4",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.14.0",
     compilerPlugin("com.github.ghik" % "silencer-plugin" % silencerVersion cross CrossVersion.full),
     "com.github.ghik" % "silencer-lib" % silencerVersion % Provided cross CrossVersion.full
   )
@@ -25,10 +25,10 @@ object AppDependencies {
   val test: Seq[ModuleID] = Seq(
     "com.github.tomakehurst" % "wiremock"                  % "2.33.2"             % scope,
     "com.typesafe.play"      %% "play-test"                % current              % scope,
-    "org.mockito"            % "mockito-core"              % "4.8.0"              % scope,
+    "org.mockito"            % "mockito-core"              % "4.8.1"              % scope,
     "org.jsoup"              % "jsoup"                     % "1.15.3"             % scope,
     "com.vladsch.flexmark"   % "flexmark-all"              % "0.62.2"             % scope,
-    "org.scalatest"          %% "scalatest"                % "3.2.13"             % scope,
+    "org.scalatest"          %% "scalatest"                % "3.2.14"             % scope,
     "org.scalatestplus.play" %% "scalatestplus-play"       % "5.1.0"              % scope,
     "org.scalatestplus"      %% "mockito-3-4"              % "3.2.10.0"           % scope,
     "org.scalatestplus"      %% "scalacheck-1-15"          % "3.2.11.0"           % scope,
