de25dbf66df83fc9d5b106896da488e8bc6061a3
Michael Yau <47248104+mywyau@users.noreply.github.com>
2023-05-04 14:55:57 +0100

    DDCE-4186 Update CSP policy, update javascript and bump dependencies (#104)
    * DDCE-4186 Update CSP policy, update javascript and bump dependencies
    
    * fix dep bump
    
    * new line

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,27 +6,27 @@ object AppDependencies {
   val scope = "test"
 
   private val hmrcMongoPlayVersion = "0.74.0"
-  private val bootstrapVersion     = "7.14.0"
+  private val bootstrapVersion     = "7.15.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28" % bootstrapVersion,
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"         % hmrcMongoPlayVersion,
     "uk.gov.hmrc"                  %% "play-allowlist-filter"      % "1.1.0",
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc"         % "6.8.0-play-28",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc"         % "7.7.0-play-28",
     "org.typelevel"                %% "cats-core"                  % "2.9.0",
     "com.digitaltangible"          %% "play-guard"                 % "2.5.0",
     "org.quartz-scheduler"         % "quartz"                      % "2.3.2",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.14.2"
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.15.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % bootstrapVersion     % scope,
     "com.github.tomakehurst" % "wiremock"                 % "2.33.2"             % scope,
     "com.typesafe.play"      %% "play-test"               % current              % scope,
-    "org.jsoup"              % "jsoup"                    % "1.15.4"             % scope,
-    "com.vladsch.flexmark"   % "flexmark-all"             % "0.64.0"             % scope,
+    "org.jsoup"              % "jsoup"                    % "1.16.1"             % scope,
+    "com.vladsch.flexmark"   % "flexmark-all"             % "0.64.4"             % scope,
     "org.scalatest"          %% "scalatest"               % "3.2.15"             % scope,
-    "org.mockito"            %% "mockito-scala-scalatest" % "1.17.12"            % scope,
+    "org.mockito"            %% "mockito-scala-scalatest" % "1.17.14"            % scope,
     "org.scalatestplus"      %% "scalacheck-1-17"         % "3.2.15.0"           % scope,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % hmrcMongoPlayVersion % scope
   )
