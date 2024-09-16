f31ba09bf93f0f24458ba71ab256a14b0391cbaf
divya-manivannan-hmrc <175815017+Divya-Manivannan-hmrc@users.noreply.github.com>
2024-09-16 16:54:57 +0100

    DDCE-4966 : ATaR- Improve the error logging format

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,12 +3,12 @@ import sbt.*
 object AppDependencies {
 
   private val hmrcMongoPlayVersion = "2.2.0"
-  private val bootstrapVersion     = "9.3.0"
+  private val bootstrapVersion     = "9.4.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"         % hmrcMongoPlayVersion,
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30" % "10.8.0",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30" % "10.11.0",
     "org.typelevel"                %% "cats-core"                  % "2.12.0",
     "com.digitaltangible"          %% "play-guard"                 % "3.0.0",
     "org.quartz-scheduler"          % "quartz"                     % "2.3.2",
