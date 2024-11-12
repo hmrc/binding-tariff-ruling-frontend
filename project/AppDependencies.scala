b288cd4b78affc4d2050f75f22df9cb086771da2
Victor O <osipovvictor1994@gmail.com>
2024-11-12 16:31:33 +0000

    DDCE-6176 - Migrate to nvmrc file (#122)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,17 +2,17 @@ import sbt.*
 
 object AppDependencies {
 
-  private val hmrcMongoPlayVersion = "2.2.0"
-  private val bootstrapVersion     = "9.4.0"
+  private val hmrcMongoPlayVersion = "2.3.0"
+  private val bootstrapVersion     = "9.5.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"         % hmrcMongoPlayVersion,
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30" % "10.11.0",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30" % "11.3.0",
     "org.typelevel"                %% "cats-core"                  % "2.12.0",
     "com.digitaltangible"          %% "play-guard"                 % "3.0.0",
     "org.quartz-scheduler"          % "quartz"                     % "2.3.2",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.17.2",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.18.1",
     "org.apache.pekko"             %% "pekko-connectors-csv"       % "1.0.2"
   )
 
