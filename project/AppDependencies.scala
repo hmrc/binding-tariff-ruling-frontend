6047bb08b4fd6dbfa0c94de3a2bed1370bcebf6a
Erick Borda <47341247+erickbm6922@users.noreply.github.com>
2025-09-08 11:08:36 +0100

    DDCE-6862 (#129)
    * Rebrand implementation on binding-tariff-ruling
    
    * Rebrand implementation on binding-tariff-ruling
    
    * Rebrand implementation on binding-tariff-ruling

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt.*
 
 object AppDependencies {
 
-  private val hmrcMongoPlayVersion = "2.6.0"
-  private val bootstrapVersion     = "9.12.0"
+  private val hmrcMongoPlayVersion = "2.7.0"
+  private val bootstrapVersion     = "10.1.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"         % hmrcMongoPlayVersion,
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30" % "12.1.0",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30" % "12.10.0",
     "org.typelevel"                %% "cats-core"                  % "2.13.0",
     "com.digitaltangible"          %% "play-guard"                 % "3.0.0",
     "org.quartz-scheduler"          % "quartz"                     % "2.5.0",
