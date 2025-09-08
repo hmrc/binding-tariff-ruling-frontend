89b00f1bb6802f3dd23cb212a8bed3a88f0da131
erickbm6922 <47341247+erickbm6922@users.noreply.github.com>
2025-09-08 11:01:25 +0100

    Rebrand implementation on binding-tariff-ruling

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val hmrcMongoPlayVersion = "2.6.0"
+  private val hmrcMongoPlayVersion = "2.7.0"
   private val bootstrapVersion     = "10.1.0"
 
   val compile: Seq[ModuleID] = Seq(
