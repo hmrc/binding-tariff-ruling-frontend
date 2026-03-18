13f6c8d1943e45ae78a5c183fa55edfd798ab6b7
erickbm6922 <47341247+erickbm6922@users.noreply.github.com>
2026-03-18 13:10:28 +0000

    DDCE-8208: Upraiding dependecies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,17 +8,17 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"         % hmrcMongoPlayVersion,
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30" % "12.29.0",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30" % "13.0.0",
     "org.typelevel"                %% "cats-core"                  % "2.13.0",
     "com.digitaltangible"          %% "play-guard"                 % "3.0.0",
-    "org.quartz-scheduler"          % "quartz"                     % "2.5.0",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.19.0"
+    "org.quartz-scheduler"          % "quartz"                     % "2.5.2",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.21.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoPlayVersion,
-    "org.jsoup"          % "jsoup"                   % "1.20.1"
+    "org.jsoup"          % "jsoup"                   % "1.22.1"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
