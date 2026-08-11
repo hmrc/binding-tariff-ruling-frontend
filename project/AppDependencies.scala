815607fb9b594c728bdeee4db245ba28e1217bc4
erickbm6922 <47341247+erickbm6922@users.noreply.github.com>
2026-08-11 10:17:59 +0100

    DDCE-8947 Replace WithUnsafeDefaultFormBinding in binding-tariff-ruling-frontend

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val hmrcMongoPlayVersion = "2.12.0"
+  private val hmrcMongoPlayVersion = "2.13.0"
   private val bootstrapVersion     = "10.7.0"
 
   val compile: Seq[ModuleID] = Seq(
